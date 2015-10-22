1e6c22ba023ba3aa51e7c98b4217539a7d8880d0
木村恒輝 <kokiiii12189527@gmail.com>
2015-10-22 10:49:51 +0900

    init commit

---
+++ db/migrate/20150923070522_create_questions.rb
@@ -0,0 +1,13 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+
+      t.integer :user_id
+      t.integer :group_id
+      t.text :text
+
+
+      t.timestamps null: false
+    end
+  end
+end
